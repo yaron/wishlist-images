a1ce69e6c0725347c8fdcca78f4755d655b876ae
Yaron Tal <yarontal12@gmail.com>
2021-09-25 19:21:42 +0200

    Add more error logging

--- src/pages/add.go
+++ src/pages/add.go
@@ -1,6 +1,7 @@
 package pages
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,11 +13,13 @@ func Add(c *gin.Context) {
 	var json utils.Image
 	if err := c.ShouldBindJSON(&json); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		log.Println("Warning: " + err.Error())
 		return
 	}
 	filename, err := utils.AddItem(json)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		log.Println("Warning: " + err.Error())
 		return
 	}
 	c.JSON(200, gin.H{"status": "Item added", "localUri": filename})
